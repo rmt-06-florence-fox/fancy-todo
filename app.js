d207f54a4dde0aafc1dfbc48efd04f6cbc6e8f09
FaishalFadhil <AchmadFaishal.Fadhil@outlook.com>
2020-11-23 12:22:25 +0700

    first int - preambule

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const router = require('./routes')
+const port = 3000
+
+app.use(express.urlencoded({extended : false}))
+app.use(express.json())
+app.use(router)
+
+app.listen(port, ()=> {
+  console.log(`app is listening to http://localhost:${port}`);
+})
