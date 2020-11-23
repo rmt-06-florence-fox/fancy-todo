cff22da5bf412375ba174d6f9bd0666af73f8b08
tiorzal <tioralmunazir@gmail.com>
2020-11-23 19:27:37 +0700

    kurang delete

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -11,16 +11,77 @@ class TodoController{
     
     try{
       const data = await Todo.create(newTodo)
-      console.log('a');
       res.status(201).json(data)
     }catch(error){
       res.status(500).json(error)
     }
   }
 
-  static findAll(req,res){
-    res.send('asad')
+  static async findAll(req,res){
+    try{
+      const todos =  await Todo.findAll()
+      res.status(200).json(todos)
+    }catch(error){
+      res.status(500).json(error)
+    }
   }
+
+  static async findOne(req,res){
+    try{
+      const id = +req.params.id
+      const todo = await Todo.findOne({where: id})
+      res.status(200).json(todo)
+    }catch(error){
+      res.status(404).json(error)
+    }
+  }
+
+  static async editPut(req,res){
+    
+    const id = req.params.id
+    const editedTodo = {
+      tittle : req.body.tittle,
+      description : req.body.description,
+      status : req.body.status,
+      due_date: req.body.due_date
+    }
+    try{
+      const data = await Todo.update(editedTodo,{where : {id:id}, returning:true})
+      if(data[0] > 0){
+        res.status(200).json(data)
+      }else{
+        res.status(404).json({error: 'Id not found'})
+      }
+    }catch (error) {
+      if(error.errors[0].type =='Validation error'){
+        res.status(400).json(error.errors[0].message)
+      } else {
+        res.status(500).json(error)
+      }
+    }
+  }
+
+  static async editPatch(req,res){
+    const id = req.params.id
+    const editedTodo = {
+      status : req.body.status
+    }
+    try{
+      const data = await Todo.update(editedTodo,{where : {id:id}, returning:true})
+      if(data[0] > 0){
+        res.status(200).json(data)
+      }else{
+        res.status(404).json({error: 'Id not found'})
+      }
+    }catch (error) {
+      if(error.errors[0].type =='Validation error'){
+        res.status(400).json(error.errors[0].message)
+      } else {
+        res.status(500).json(error)
+      }
+    }
+  }
+  
 }
 
 module.exports = TodoController
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,6 +3,11 @@ const {TodoController} = require('../controllers')
 
 router.post('/', TodoController.create)
 router.get('/', TodoController.findAll)
+router.get('/:id', TodoController.findOne)
+router.put('/:id', TodoController.editPut)
+router.patch('/:id', TodoController.editPatch)
+
+
 
 
 module.exports = router
