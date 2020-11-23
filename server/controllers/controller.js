a177fc93a00d44f3c0c9cafe68bc47e5b8d69035
TommySus <tommysusanto77@gmail.com>
2020-11-23 21:43:44 +0700

    belum-tinggal-validasi

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -50,8 +50,111 @@ class Controller {
         })
     }
 
-    static listTodos(req,res) {
-        res.send("hello")
+    static createTodo(req,res) {
+        const obj = {
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            due_date: req.body.due_date,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        Todo.create(obj)
+        .then( data => {
+            res.status(201).json({
+                title: data.title,
+                description: data.description,
+                status: data.status,
+                due_date: data.due_date
+            })
+        })
+        .catch(error => {
+            res.status(500).json({message: "internal server error"})
+        })
+    }
+
+    static listTodo(req,res){
+        Todo.findAll()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(error => {
+            res.status(500).json({message: "internal server error"})
+        })
+    }
+
+    static findTodoById(req,res){
+        Todo.findOne({where: {id: req.params.id}})
+        .then(data => {
+            if (data){
+                res.status(200).json({
+                    title: data.title,
+                    description: data.description,
+                    status: data.status,
+                    due_date: data.due_date
+                })
+            } else {
+                res.status(404).json({message: "id not found!"})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({message: "internal server error"})
+        })
+    }
+
+    static updateTodo(req,res){
+        Todo.update({
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            due_date: req.body.due_date
+        },{where: {id: req.params.id}})
+        .then(data => {
+            res.status(200).json({
+                title: data.title,
+                description: data.description,
+                status: data.status,
+                due_date: data.due_date
+            })
+        })
+        .catch(error => {
+            res.status(500).json({message: "internal server error"})
+        })
+    }
+
+    static updateStatusTodo(req,res){
+        Todo.update({
+            status: req.body.status,
+        },{where: {id: req.params.id}})
+        .then(data => {
+            if (data){
+                res.status(200).json({
+                    title: data.title,
+                    description: data.description,
+                    status: data.status,
+                    due_date: data.due_date
+                })
+            }else {
+
+            }
+        })
+        .catch(error => {
+            res.status(500).json({message: 'internal server error'})
+        })
+    }
+
+    static deleteTodo(req,res){
+        Todo.destroy({where: {id: req.params.id}})
+        .then( data => {
+            if (data){
+                res.status(200).json({message: 'todo success to delete'})
+            } else {
+                res.status(404).json({message: 'id not found!'})
+            }
+        })
+        .catch( error => {
+            res.status(500).json({message: 'internal server error'})
+        })
     }
 }
 
--- server/routes/todos.js
+++ server/routes/todos.js
@@ -2,8 +2,14 @@ const express = require('express')
 const router = express.Router()
 const Controller = require('../controllers/controller')
 
-router.get("/", Controller.listTodos)
 
+
+router.get("/", Controller.listTodo)
+router.post("/create", Controller.createTodo)
+router.get("/:id", Controller.findTodoById)
+router.post("/delete/:id", Controller.deleteTodo)
+router.put("/update/:id", Controller.updateTodo)
+router.patch("/update/:id", Controller.updateStatusTodo)
 
 
 module.exports = router
