7d38f484f6a1296e04022adf07a4821f31dcf717
alfatio <alfatioutama@gmail.com>
2020-11-23 13:08:12 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('Hello from app.js')
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`app running on http://localhost:3000`)
+})
