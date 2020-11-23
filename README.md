522f640a4fcc1be37578aa3292cafdeb49dd43de
unknown <wicaksanapratama@gmail.com>
2020-11-23 15:23:32 +0700

    Add Documentation in Readme, Post still return pending promise but data is succesfully stored

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fancy-todo
+
+
+link postman doc:
+
+https://web.postman.co/collections/13589671-b2575a64-a1b4-4180-b265-fd32a92d3fc6?version=latest&workspace=dbdcc224-23df-4bcd-82b4-cac8c24b53f0#c757b49f-d686-4072-83e1-284b4b42ebfd
--- controllers/controller-todo.js
+++ controllers/controller-todo.js
@@ -1,10 +1,27 @@
+const { TodoList } = require ('../models')
+
 class ControllerTodo {
     static async get (req, res) {
-        
+       try {
+            const list = TodoList.findAll()
+            res.status(200).json({list})
+       } catch (err) {
+           console.log(err)
+            res.status(500).json({err})
+       }
     }
 
     static async post (req, res) {
-        
+        try {
+            const list = TodoList.create(req.body, {
+                returning: true
+            })
+            console.log(list)
+            res.status(200).json({list})
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({message:'error loading page'})
+        }
     }
     
     static async getId (req, res) {
