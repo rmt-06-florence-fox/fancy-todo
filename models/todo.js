8f967eae315beb863584c0b2fe98d579dc5b0207
arulabdulaziz <rulz.matrixs@gmail.com>
2020-11-23 12:56:40 +0700

    first

---
+++ models/todo.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  todo.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    due_date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'todo',
+  });
+  return todo;
+};
