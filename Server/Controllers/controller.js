9660f0364e447194c31b6105d9aacbc358823859
RizkyMaulita <rizky.maulita@gmail.com>
2020-11-24 17:16:16 +0700

    menambahkan rest api zomato

--- Server/Controllers/controller.js
+++ Server/Controllers/controller.js
@@ -1,6 +1,7 @@
 const TodoController = require('./todoController')
 const UserController = require('./userController')
 const axios = require('axios')
+require('dotenv').config()
 
 class Controller{
   static async getQuotes(req, res){
@@ -11,6 +12,25 @@ class Controller{
         method: "GET"
       })
       res.status(200).json({result: quotes.data})
+    }catch(err){
+      res.status(500).json(err)
+    }
+  }
+  static async getRestaurants(req, res){
+    try{
+      console.log('--- Get Restaurants ---');
+      const dataRestaurants = await axios({
+        url:'https://developers.zomato.com/api/v2.1/collections?city_id=74',
+        method: 'GET',
+        params:{
+          'city_id': 74
+        },
+        headers:{
+          'user-key': process.env.ZOMATO_key
+        }
+      })
+      console.log(dataRestaurants);
+      res.status(200).json({result: dataRestaurants.data.collections})
     }catch(err){
       res.status(500).json(err)
     }
--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -6,6 +6,7 @@ const {Controller} = require('../Controllers/controller')
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/quotes', Controller.getQuotes)
+router.get('/restaurants', Controller.getRestaurants)
 router.use('/todos', todoRouter)
 
 module.exports = router
