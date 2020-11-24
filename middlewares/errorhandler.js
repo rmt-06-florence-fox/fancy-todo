03e937e284479eb606133d760f91c4c3a5267a65
taufikikk <taufikikk@gmail.com>
2020-11-24 18:12:56 +0700

    fix Authentication Token

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -1,5 +1,4 @@
 module.exports = (err, req, res, next) => {
-    console.log("ini error handler");
     console.log(err);
     if(err.status){
         res.status(err.status).json({
@@ -9,7 +8,9 @@ module.exports = (err, req, res, next) => {
         res.status(400).json({
             message: err.errors[0].message
         })
+    } else if (err.name == 'JsonWebTokenError'){
+        res.status(500).json(err.message)
     } else {
-        res.status(500).error
+        res.status(500).json(err)
     }
 }
