283b090d6b2f08fc4b86db298967c65d60260081
RizkyMaulita <rizky.maulita@gmail.com>
2020-11-24 15:43:11 +0700

    menambahkan rest api quote

--- Server/Controllers/controller.js
+++ Server/Controllers/controller.js
@@ -1,7 +1,24 @@
 const TodoController = require('./todoController')
 const UserController = require('./userController')
+const axios = require('axios')
+
+class Controller{
+  static async getQuotes(req, res){
+    try{
+      console.log('--- Get Quotes ---');
+      const quotes = await axios({
+        url: 'https://quote-garden.herokuapp.com/api/v2/quotes?page=1&limit=10',
+        method: "GET"
+      })
+      res.status(200).json({result: quotes.data})
+    }catch(err){
+      res.status(500).json(err)
+    }
+  }
+}
 
 module.exports = {
+  Controller,
   TodoController,
   UserController
 };
--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const todoRouter = require('./todo')
 const {UserController} = require('../Controllers/controller')
+const {Controller} = require('../Controllers/controller')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.get('/quotes', Controller.getQuotes)
 router.use('/todos', todoRouter)
 
 module.exports = router
