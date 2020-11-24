b6949eda1585080803666940cc20711c92730b65
tiorzal <tioralmunazir@gmail.com>
2020-11-24 12:44:08 +0700

    nambah middleware authentication

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,8 +1,27 @@
 const JwtHelper = require('../helper/jwtHelper')
+const { User } = require('../models')
 
-module.exports = (req,res,next)=>{
+module.exports = async (req,res,next)=>{
   const rawtoken = req.headers.access_token;
-  console.log(JwtHelper.decodeToken(rawtoken));
-  console.log('test');
-  next();
+  let access_token
+  try {
+    access_token = JwtHelper.decodeToken(rawtoken)
+  } catch (error) {
+    res.status(401).json({message: 'please login first'})
+  }
+
+  try {
+    if(access_token){
+      const user = await User.findOne({where:{id: access_token.id}})
+      if(user){
+        next();
+      } else res.status(401).json({message: 'please login first'})
+    }else{
+      res.status(401).json({message: 'please login first'})
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+
+  // next();
 }
