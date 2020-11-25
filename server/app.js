6fb4b6916ae4db053509ba303e210eb301e2af7f
dimasgaluhss <dimasgaluh71@gmail.com>
2020-11-26 00:08:09 +0700

    Jquery CRUD

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
+
+const cors = require('cors')
 const express = require('express');
 const app = express();
 const port = 3000;
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler')
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/', router);
