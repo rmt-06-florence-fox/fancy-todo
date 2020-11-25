1bfc513ad11fb85fca1a9547eaede105c65f7eec
reyhanprtrs <reyhanprtrs@gmail.com>
2020-11-26 00:00:17 +0700

    add edit feature

--- client/assets/helper.js
+++ client/assets/helper.js
@@ -3,14 +3,16 @@ const URL = "http://localhost:3000/"
 $(document).ready(() => {
   // membedakan user dengan access token
   if (localStorage.getItem('accessToken')) showMainPage()
+  // else if (localStorage.getItem('accessToken') && editTodo()) showEditPage()
   else showSignInPage()
+
   $('#signin').click((e) => {
     e.preventDefault()
     doSignIn()
   })
   $('#add-todo').on('submit', (e) => {
     e.preventDefault()
-    createTodos()
+    createTodo()
   })
   $('#signout').click(() => {
     doSignOut()
@@ -21,14 +23,24 @@ function showSignInPage() {
   $('#signin-container').show()
   $('#main-page-container').hide()
   $('#signout').hide()
+  $('#edit-page-container').hide()
 }
 
 function showMainPage() {
   $('#signin-container').hide()
   $('#main-page-container').show()
   $('#signout').show()
+  $('#edit-page-container').hide()
   fetchTodos()
 }
+
+  function showEditPage(id) {
+    $('#signin-container').hide()
+    $('#main-page-container').hide()
+    $('#signout').hide()
+    $('#edit-page-container').show()
+    editTodo(id)
+  }
 
 function doSignIn() {
   const email = $('#input-email').val()
@@ -56,10 +68,6 @@ function doSignIn() {
     })
 }
 
-// function displayMainPage() {
-
-// }
-
 function fetchTodos() {
   $('#todo-list').empty()
   $.ajax({
@@ -78,6 +86,7 @@ function fetchTodos() {
             <h5>${e.description}</h5>
             <h5>${e.status}</h5>
             <h5>${e.dueDate}</h5>
+            <button type="submit" onclick="showEditPage(${e.id})" class="btn btn-primary">Edit</button>
             <button type="submit" onclick="deleteTodo(${e.id})">Delete</button>
           </div>
         </div>`)
@@ -88,7 +97,7 @@ function fetchTodos() {
     })
 }
 
-function createTodos() {
+function createTodo() {
   const title = $('#input-title').val()
   const description = $('#input-description').val()
   let date = $('#input-date').val()
@@ -110,6 +119,42 @@ function deleteTodo(id) {
     headers: { accessToken: localStorage.getItem('accessToken') }
   })
     .done(() => fetchTodos())
+    .fail(err => console.log(err))
+}
+
+function updateTodo(id) {
+  const title = $('#edit-title').val()
+  const description = $('#edit-description').val()
+  const status = $('#edit-status').val()
+  let date = $('#edit-date').val()
+  $.ajax({
+    url: `${URL}todos/${id}`,
+    method: 'PUT',
+    headers: { accessToken: localStorage.getItem('accessToken') },
+    data: { title, description, status, dueDate: formatDate(date) }
+  })
+    .done(data => showMainPage())
+    .fail(err => console.log(err))
+}
+
+function editTodo(id) {
+  $.ajax({
+    url: `${URL}todos/${id}`,
+    method: 'GET',
+    headers: {
+      accessToken: localStorage.getItem('accessToken')
+    }
+  })
+    .done(result => {
+      $('#edit-title').val(result.title)
+      $('#edit-description').val(result.description)
+      $('#edit-status').val(result.status)
+      $('#edit-date').val(result.dueDate)
+      $('#edit-todo').on('submit', (e) => {
+        e.preventDefault()
+        updateTodo(id)
+      })
+    })
     .fail(err => console.log(err))
 }
 
