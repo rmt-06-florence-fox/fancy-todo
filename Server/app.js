8e56a419dbb41be44f9470915651b176699c7159
RizkyMaulita <rizky.maulita@gmail.com>
2020-11-25 16:33:58 +0700

    regist dan login

--- Server/app.js
+++ Server/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
 const router = require('./Routes/index')
 const errorHandler = require('./middlewares/errorHandler')
 
+app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use('/', router)
