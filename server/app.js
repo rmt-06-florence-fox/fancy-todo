cbb4627218501767f843e129110cef2424764760
TommySus <tommysusanto77@gmail.com>
2020-11-25 19:33:00 +0700

    add-update-delete-belum

--- server/app.js
+++ server/app.js
@@ -3,12 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const routes = require('./routes/index')
+const cors = require('cors')
 
 
 
 app.use(express.urlencoded({extended:true}))
 
-
+app.use(cors())
 app.use("/",routes)
 
 app.listen(port, ()=> {
