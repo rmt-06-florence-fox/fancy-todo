1d506f9425c9a75782c1dee6ae677404832c6d48
fajarrrX <fajarrr30@gmail.com>
2020-11-25 22:57:32 +0700

    add client register and login, haven't handled other client features yet

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = 3000;
 const routes = require(`./routes`);
 const errorHandler = require("./middlewares/errorHandler");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(`/`, routes);
