6b07761e4e7d3ca745031d8330ac03409902142b
RizkyMaulita <rizky.maulita@gmail.com>
2020-11-28 01:51:42 +0700

    add zomato page fetch zomato add quote page fetch quote fixed display

--- Client/assets/helper.js
+++ Client/assets/helper.js
@@ -1,3 +1,5 @@
+const baseURL = 'http://localhost:3000/'
+
 function onSignIn(googleUser) {
   var googleToken = googleUser.getAuthResponse().id_token;
   $.ajax({
@@ -100,8 +102,11 @@ function showMainPage(){
   $('#regist-page').hide()
   $('#btn-logout').show()
   $('#main-page').show()
+  $('#create-todo').hide()
   $('#edit-todo').hide()
   fetchDataTodoList()
+  quote()
+  zomato()
 }
 function showEditPage(data){
   $('.navbar').show()
@@ -140,12 +145,16 @@ function showEditPage(data){
           </div>
         </div>
         <div class="form-group row">
-          <div class="col-sm-10">
-            <input type="submit" class="btn btn-primary" value="Edit">
+          <div class="mx-auto">
+            <input type="submit" class="btn btn-edit" value="Edit">
           </div>
         </div>
       </form>
   `)
+}
+function showCreateTodo(){
+  showMainPage()
+  $('#create-todo').show()
 }
 function createTodo(){
   const title = $('#inputTitle').val()
@@ -218,6 +227,18 @@ function updateData(id){
     .fail(xhr => console.log(xhr))
     .always(_ => $('#edit-todo').trigger('reset'))
 }
+function patchData(id){
+  $.ajax({
+    method: 'PATCH',
+    url: baseURL + 'todos/' + id,
+    headers:{
+      access_token: localStorage.getItem('access_token')
+    },
+    data: { status: 'done' }
+  })
+    .done(response => showMainPage())
+    .fail(xhr => console.log(xhr))
+}
 function fetchDataTodoList(){
   $.ajax({
     method: 'GET',
@@ -228,7 +249,6 @@ function fetchDataTodoList(){
   })
     .done(response =>{
       $('#todo-list').empty()
-      // console.log(response);
       response.data.forEach((e)=>{
         $('#todo-list').append(`
         <div class = "col mb-4">
@@ -250,14 +270,77 @@ function fetchDataTodoList(){
                 <td>${e.due_date}</td>
               </tr>
             </table>
-            <div class="card-body">
-              <a class="btn btn-primary" href="#" id="btn-edit" onclick = "editData(${e.id})">Edit</a>
-              <a class="btn btn-danger" href="#" id="btn-delete" onclick = "deleteData(${e.id})">Delete</a>
+            <div class="card-body mx-auto">
+              <a class="btn far fa-check-square fa-2x" href="#" id="btn-edit" onclick = "patchData(${e.id})"></a>
+              <a class="btn far fa-edit fa-2x" href="#" id="btn-edit" onclick = "editData(${e.id})"></a>
+              <a class="btn far fa-trash-alt fa-2x" href="#" id="btn-delete" onclick = "deleteData(${e.id})"></a>
             </div>
           </div>
         </div>
         `)
       })
+    })
+    .fail(xhr => console.log(xhr))
+}
+function quote(){
+  $.ajax({
+    method: 'GET',
+    url: baseURL + 'quotes'
+  })
+    .done(response => {
+      console.log(response, '<<<isi quote');
+      $('#list-quote').empty()
+      response.result.quotes.forEach((e)=>{
+        $('#list-quote').append(`
+          <div class="card-body">
+            <h6> <i>"${e.quoteText}"</i>  </h6>
+            <p class="text-right"><i>'${e.quoteAuthor}'</i></p>
+          </div>
+        `)
+      })
+    })
+    .fail(xhr => console.log(xhr))
+}
+function zomato(){
+  $.ajax({
+    method: 'GET',
+    url: baseURL + 'restaurants'
+  })
+    .done(response=>{
+      console.log(response);
+      console.log(response.data.length , '<<< banyaknya data');
+      let gatcha = Math.floor(Math.random() * response.data.length)
+      console.log(gatcha);
+      let data = response.data[gatcha].restaurant
+      console.log(data, '<<< data restaurants hasil gatcha');
+      $('#fetchZomato').empty()
+      $('#fetchZomato').append(`
+        <div class="card-header header-zomato-list">
+        <h6 class="card-title text-center">${data.name}</h6>
+      </div>
+      <div class="card-body table-zomato">
+        <img src="${data.featured_image}" class="card-img-top mx-auto img-zomato" alt="">
+        <table class="table table-borderless">
+          <th>Location</th>
+          <td>${data.location.address}</td>  
+        </tr>
+        <tr>
+          <th>Cuisines</th>
+          <td>${data.cuisines}</td> 
+          
+        </tr>
+        <tr>
+          <th>Open Hours</th>
+          <td>${data.timings}</td>
+        </tr>
+        <tr>
+          <th>Rating</th>
+          <td>${data.user_rating.aggregate_rating}</td>
+          
+        </tr>
+        </table>
+        </div>
+      `)
     })
     .fail(xhr => console.log(xhr))
 }
