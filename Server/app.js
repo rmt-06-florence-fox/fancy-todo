6a8215b7b1d9e7e38a9c833f7beeb14d40113bcd
razaqisama <razaqisama@gmail.com>
2020-11-28 14:47:56 +0700

    coba deploy

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require('./routers/index');
 const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler');
