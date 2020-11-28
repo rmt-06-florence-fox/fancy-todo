c5bd88d9fc0f90e8d9f75bd03129f8862bccb435
FaishalFadhil <AchmadFaishal.Fadhil@outlook.com>
2020-11-28 17:35:31 +0700

    first

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const router = require('./routes')
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const errorHandler = require('./middleware/errorHandler')
 
