e5d1951c2a17df393ccebebccabeb86ad70f5aab
agnespravida <agnespravida27@gmail.com>
2020-11-28 11:04:39 +0700

    1. Change register and login form to card 2. Creating navbar in main page

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,6 @@
 const { User } = require("../models/index")
 const comparePassword = require("../helpers/comparepassword")
 const generateToken = require("../helpers/generateToken")
-const sendEmail = require("../nodemailer")
 
 class UserController {
   static async register(req, res, next){
@@ -14,15 +13,6 @@ class UserController {
         password: req.body.password
       }
       const user = await User.create(payload)
-      
-      sendEmail(user.email, "Account Verification", "Test verification", (err, info) => {
-        if(err){
-          throw(err)
-        }
-        else {
-          console.log("Successfully send email")
-        }
-      })
       res.status(201).json(user)
 
     } catch (error) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,26 @@ const express = require("express")
 const router = express.Router()
 const todoRouter = require("./todo.js")
 const UserController = require("../controllers/user")
+const axios = require("axios")
+
 
 router.post("/register", UserController.register)
 router.post("/login", UserController.login)
 router.use("/todos", todoRouter)
+router.get("/quotes", (req, res, next) => {
+  axios({
+    url: "https://favqs.com/api/qotd",
+    method: "GET"
+  })
+    .then(response => {
+      res.status(200).json({
+        quotes: response.data.quote.body,
+        author: response.data.quote.author
+      })
+    })
+    .catch(err => {
+      next(err)
+    })
+})
 
 module.exports = router
