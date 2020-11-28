bf395536f63a5ff4c4553d5d254fafc9950f946a
marcellasyw <marcella.septania0@gmail.com>
2020-11-28 12:39:35 +0700

    Belum selesai

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const port = 3000
 const routes = require('./routes/index')
 const errHandler = require('./helpers/errorhandler')
 const cors = require('cors')
-
 app.use(cors())
 app.use(express.urlencoded({extended : false}))
 app.use(express.json())
