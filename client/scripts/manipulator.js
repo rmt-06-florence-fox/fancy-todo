95d5a823a9177019d428214d5e879eac3002b893
hndrbudisantoso@gmail.com <hndrbudisantoso@gmail.com>
2020-11-28 08:34:23 +0700

    redesign page

--- client/scripts/manipulator.js
+++ client/scripts/manipulator.js
@@ -1,8 +1,3 @@
 $(document).ready( _ => {
-    localStorage.getItem('token') ? showContent() : showLogin() 
-    
-    $('#register-anchor').on('click', showRegister)  
-    $('#login-form').on('submit', loginHandler)
-    $('#register-form').on('submit', registerHandler)
-    $('#logoutBtn').on('click', logoutHandler)
+    beforeLogin()
 })
