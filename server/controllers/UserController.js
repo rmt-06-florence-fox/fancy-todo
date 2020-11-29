d9fe668f34a8dcd88bd3873c61bd82752f71d75a
icanq <icanq@.localdomain>
2020-11-29 13:36:16 +0700

    fix google sign in

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,8 @@
 const { User } = require("../models");
 const { compare } = require("../helpers/passwordHandler");
-const { generateToken, verifyToken } = require("../helpers/tokenHandler");
+const { generateToken } = require("../helpers/tokenHandler");
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(process.env.CLIENT_ID)
 class UserController {
 
   //create
@@ -55,40 +57,41 @@ class UserController {
   }
 
   //gsignin
-  static loginGoogle(req, res, next) {
-    // Verify Token
-    // Dapetin Token dari Client
-    let {google_access_token} = req.body;
-    const client = new OAuth2Client(process.env.CLIENT_ID);
-    let email = '';
-    // Verify Google Token Berdasarkan Client ID
-    client.verifyIdToken({
-      idToken: google_access_token,
-      audience: process.env.CLIENT_ID,
-    })
-    .then((ticket) => {
-      let payload = ticket.getPayload();
-      email = payload.email;
-      return User.findOne({where: {email: payload.email}});
-    })
-    .then((user) => {
-      if (user) {
-        return user;
-      } else {
-        var userObj = {
-          email,
-          password: process.env.PASSWORD
+  static async loginGoogle(req, res, next) {
+    try {
+      const ticket = await client.verifyIdToken({
+        idToken: req.body.googleToken,
+        audience: process.env.CLIENT_ID
+      })
+      const payload = ticket.getPayload();
+      console.log(payload)
+      const user = await User.findOne({
+        where: {
+          email: payload.email
         }
-        return User.create(userObj);
+      })
+      if (!user) {
+        const gUser = await User.create({
+          username: `${payload.given_name}${payload.family_name}`,
+          email: payload.email,
+          password: process.env.PASSWORD
+        })
+        const access_token = generateToken({
+          id: gUser.id,
+          email: gUser.email,
+        });
+        res.status(200).json({ access_token })
+      } else {
+        const access_token = generateToken({
+          id: user.id,
+          email: user.email,
+        });
+        res.status(200).json({ access_token })
       }
-    })
-    .then((dataUser) => {
-      let access_token = signToken({id: dataUser.id, email: dataUser.email});
-      return res.status(200).json({access_token});
-    })
-    .catch((err) => {
-      next(err);
-    });
+    } catch (err) {
+      console.log(err)
+      next(err)
+    }
   }
 }
 
