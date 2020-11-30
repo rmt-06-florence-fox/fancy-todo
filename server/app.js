51775398f84275b932f190e0038cce1326b0ae25
Rudy <hactiv8rud@gmail.com>
2020-11-30 17:16:23 +0700

    deploy prep

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const errorHandler = require("./middleware/errorHandler");
 const routes = require("./routes");
 
