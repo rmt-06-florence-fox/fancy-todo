87cc43626c542d74db4ab4f46145f32d701bf530
muchsin28 <hab@muchsin.id>
2020-11-30 18:35:11 +0700

    fixed routingan ke heroku

--- client/index.js
+++ client/index.js
@@ -28,7 +28,7 @@ const login=()=>{
   .done(response =>{
     localStorage.setItem('access_token', response.access_token)
     localStorage.setItem('name', response.name)
-    localStorage.setItem('name', response.email)
+    localStorage.setItem('email', response.email)
     $("#user-name").text(localStorage.name)
     $("#user-email").text(localStorage.email)
     showMainPage()
