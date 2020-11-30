2240dfd847ce2917e1abedf7a370cba5e3334213
andhikam <andhikamuttaqien@gmail.com>
2020-12-01 06:27:02 +0700

    Deployment Exercise

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config();
+}
+
 const express = require('express');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
