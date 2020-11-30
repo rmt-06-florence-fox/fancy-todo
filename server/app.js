20e20c087bd493c14d2a5236ea67d0928e522228
taufikikk <taufikikk@gmail.com>
2020-11-30 15:47:03 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
-require('dotenv').config()
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 const errorhandler = require('./middlewares/errorhandler')
 const cors = require('cors')
