18229d33627f4d0140322c8078c6841765aecec9
FaishalFadhil <AchmadFaishal.Fadhil@outlook.com>
2020-11-30 20:53:24 +0700

    push

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const {TodoController} = require('../controller')
-const authorization = require('../middleware/Authorization')
+const Authorization = require('../middleware/Authorization')
  
 router.get('/', TodoController.show)
 router.post('/', TodoController.create)
-router.use('/:id', authorization)
+router.use('/:id', Authorization)
 router.get('/:id', TodoController.seeList)
 router.put('/:id', TodoController.update)
 router.patch('/:id', TodoController.patch)
