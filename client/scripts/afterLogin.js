d4a65583eb6e7c49773f50347130eb4640992874
hndrbudisantoso@gmail.com <hndrbudisantoso@gmail.com>
2020-12-20 21:33:22 +0700

    1. layouting (ngerapihin) 2. re-coloring 3. add tooltip on buttons 4. sorting based on status

--- client/scripts/afterLogin.js
+++ client/scripts/afterLogin.js
@@ -7,9 +7,10 @@ function logoutHandler() {
 function showContent() {
   clearContent()
   fetchContents()
+  fetchQuote()  
   setInterval(() => {
-    fetchQuote()
-  }, 5200)
+    fetchQuote()  
+  }, 6*15200)
   $('#login-page').hide()
   $('#register-page').hide()
   $('#edit-page').hide()
@@ -59,45 +60,56 @@ function fetchContents() {
   const token = localStorage.getItem('token')
   if (token) {
     $.ajax({
-        url: server + '/todos',
-        method: 'GET',
-        headers: { token }
-    }).done(response => {
-          //console.log(response)
-        response.forEach(todo => {
-          let {id, title, description, due_date, status} = todo
-          due_date = due_date.split('T')[0]
-          //console.log(status)
-          let btn
-          if(status === 'done') {
-            btn = "undone"
-          }
-          else {
-            btn = 'done'
-          } 
-          $('#content').append(`
-            <div class="card-deck border-dark m-5 col-sm-3">
-            <div class="card-header row">
-              <button onclick="alterStatus(${id}, '${btn}')" class="col-3 btn-dark m-1 text-light amterial-icons"> ${btn} </button>
-              
-              <button onclick="showEdit(${id},'${title}', '${description}', '${due_date}')" class="col-3 btn-warning text-dark m-1 material-icons">edit</button>
-
-              <button onclick="deleteTodo(${id})" class="col-3 btn-danger text-light m-1 material-icons">delete_forever</button> 
-  
-            <h4 class="col-8"> ${title}</h4></div>
-
-            <div class="card-body bg-info text-white">${description}</div>
-            
-            <div class="card-footer">due date : ${due_date} </div>
-            </div>
-        `);
-        })
-      }).fail(xhr => {
-          //console.log(xhr)
-          const errors = xhr.responseJSON.errors
-          //console.log(errors)
-          showError(errors)
+      url: server + '/todos',
+      method: 'GET',
+      headers: { token }
+    })
+    .done(response => {
+      response.sort(function(a){
+        if (a.status == 'undone') return -1
+        return 0
       })
+      response.forEach(todo => {
+        let {id, title, description, due_date, status} = todo
+        due_date = due_date.split('T')[0]
+        //console.log(status)
+        let btn
+        if (status === 'done') btn = "undone"
+        else btn = 'done'
+        $('#content').append(`
+          <div class="card p-0 col-md-3 col-12 m-2 text-center"">
+            <div class="card-header d-flex justify-content-center">
+              <button 
+              onclick="alterStatus(${id}, '${btn}')" class="col-3 btn-dark m-1 text-light text-center material-icons"
+              data-toggle="tooltip" title="change status to be ${btn}"
+              > ${btn} </button>
+              
+              <button 
+              onclick="showEdit(${id},'${title}', '${description}', '${due_date}')" 
+              class="col-3 btn-warning text-dark text-center m-1 material-icons"
+              data-toggle="tooltip" title="Edit it"
+              >edit</button>
+
+              <button
+              onclick="deleteTodo(${id})"
+              class="col-3 btn-danger text-light m-1 text-center material-icons"
+              data-toggle="tooltip" title="Delete it"         
+              >delete_forever</button> 
+            </div>
+            <div class="card-header">
+              <h4> ${title}</h4>
+            </div>
+
+            <div class="card-body">${description}</div>
+            <div class="card-footer">due date : ${due_date} </div>
+          </div>
+      `);
+      })
+    })
+    .fail(xhr => {
+      const errors = xhr.responseJSON.errors
+      showError(errors)
+    })
   }
 }
 function showAdd(){
