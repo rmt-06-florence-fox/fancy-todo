1d7993b5d0435d384685de9e5f8ce25989a8ef10
muchsin28 <hab@muchsin.id>
2021-01-01 13:52:10 +0700

    Revert "update"
    This reverts commit 101fe2ffb25f06cf5bc2bc39a7454a80450bae6c.
    Salah Commit

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 
 function generateToken(payload){
-  return jwt.sign(payload, "CocaColaZero")
+  return jwt.sign(payload,process.env.SECRET)
 }
 
 function verifyToken(token){
-  return jwt.verify(token, "CocaColaZero")
+  return jwt.verify(token,process.env.SECRET)
   
 }
 
