afa594e629f9ff3c47c0b769ce9de69318c48e71
Jalu Wibowo Aji <jaluwibowoaji@SekolahMus-MacBook-Pro-2.local>
2023-08-23 23:27:23 +0700

    chore: add log for weather api

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -5,13 +5,16 @@ class ApiController {
         const lat = req.headers.latitude || "-6.21462"
         const lon = req.headers.longitude || "106.84513"
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER}`
+        console.log(url, ' url')
         try {
             const result = await axios({
                 method: 'GET',
                 url
             })
+            console.log(result.data, ' result')
             res.status(200).json(result.data)
         } catch (error) {
+            console.log(error, ' error')
             next(error)    
         }
     }
