51396d62f68b24d9c6ab5ef27d5f52a8628954cf
Jalu Wibowo Aji <jaluwibowoaji@SekolahMus-MacBook-Pro-2.local>
2023-08-23 23:44:35 +0700

    fix: lat and lon null

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -3,12 +3,10 @@ const axios = require('axios')
 class ApiController {
     static async weatherToday (req, res, next) {
         let lat = "-6.21462"
-        // if (req.headers.latitude) lat = req.headers.latitude
+        if (req.headers.latitude && req.headers.latitude !== 'null') lat = req.headers.latitude
         let lon = "106.84513"
-        // if (req.headers.longitude) lon = req.headers.longitude
+        if (req.headers.longitude && req.headers.longitude !== 'null') lon = req.headers.longitude
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER}`
-        console.log(req.headers, ' headers')
-        console.log(url, ' url')
         try {
             const result = await axios({
                 method: 'GET',
